6654f75d2071f3578697c1d3067484e01100cc4b
Brandon Mason <bjm241@txstate.edu>
2025-02-14 12:12:45 -0600

    Clicking and dragging launches the ball in the opposite direction as the line. Start point of the line follows the location of the ball. SCRUM-13

--- Assets/Scripts/Player/LineRendererController.cs
+++ Assets/Scripts/Player/LineRendererController.cs
@@ -4,28 +4,46 @@ using UnityEngine.InputSystem;
 public class LineRendererController : MonoBehaviour
 {
     public LineRenderer lineRenderer;
-    private Vector3 startPos;
     private Vector3 endPos;
     private ClickAndDrag clickAndDrag;
+    private LaunchWithDrag launchWithDrag;
 
     void Start()
     {
         clickAndDrag = gameObject.GetComponent<ClickAndDrag>();
-        startPos = clickAndDrag.getStartPos();
+        launchWithDrag = gameObject.GetComponent<LaunchWithDrag>();
+        lineRenderer.enabled = false;
         endPos = clickAndDrag.getEndPos();
+        lineRenderer.SetPosition(0, gameObject.transform.position);
+        lineRenderer.SetPosition(1, endPos);
     }
 
     private void Update()
     {
         if (lineRenderer != null)
         {
-            // Changes the state of the line in real time
-            if(Mouse.current.leftButton.IsPressed()) {
-                endPos = clickAndDrag.getEndPos();
-            }
+            if(launchWithDrag.rb.linearVelocityX == 0 && launchWithDrag.rb.linearVelocityY == 0) {
+                // Changes the state of the line in real time
+                if(Mouse.current.leftButton.IsPressed()) {
+                    endPos = clickAndDrag.getEndPos();
+                }
 
-            lineRenderer.SetPosition(0, startPos);
-            lineRenderer.SetPosition(1, endPos);
+                // Set the start and end points and visibility of the Line Renderer
+                if(Mouse.current.leftButton.wasPressedThisFrame) {
+                    lineRenderer.enabled = true;
+
+                    // startPos = clickAndDrag.getStartPos();
+                } else if(Mouse.current.leftButton.wasReleasedThisFrame) {
+                    lineRenderer.enabled = false;
+
+                    // startPos = clickAndDrag.getStartPos();
+                    endPos = clickAndDrag.getEndPos();
+                }
+                lineRenderer.SetPosition(0, gameObject.transform.position);
+                lineRenderer.SetPosition(1, endPos);
+            } else {
+                lineRenderer.enabled = false;
+            }
         }
     }
 }
