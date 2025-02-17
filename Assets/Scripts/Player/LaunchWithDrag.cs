9b76f5a873a7f26ffdb91910a9b7234d64c6a130
Brandon Mason <bjm241@txstate.edu>
2025-02-16 18:26:27 -0600

    Fixed Bugs: Clicking anywhere on the board would move the ball after the first normal launch. The mouse must hover the ball before being launched. SCRUM-13

--- Assets/Scripts/Player/LaunchWithDrag.cs
+++ Assets/Scripts/Player/LaunchWithDrag.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class LaunchWithDrag : MonoBehaviour
 {
     private Rigidbody2D rb;
     private ClickAndDrag clickAndDrag;
-    private Vector3 endPos;
-    private Vector3 startPos;
-
     [SerializeField]
     private float forceAmount;
-
     // Added for testing purposes. Makes the variables show up in component view.
     [SerializeField]
     private float mass;
@@ -23,8 +18,6 @@ public class LaunchWithDrag : MonoBehaviour
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         clickAndDrag = gameObject.GetComponent<ClickAndDrag>();
-        startPos = clickAndDrag.getStartPos();
-        endPos = clickAndDrag.getEndPos();
         mass = rb.mass;
         linearDamping = rb.linearDamping;
         angularDamping = rb.angularDamping;
@@ -32,36 +25,36 @@ public class LaunchWithDrag : MonoBehaviour
 
     void Update()
     {
-        if(rb != null) {
-            if(!isMoving()) {
-                // Same kind of logic for inputs in LineRenderer.
-                if(Mouse.current.leftButton.IsPressed()) {
-                    endPos = clickAndDrag.getEndPos();
-                }
-
-                if(Mouse.current.leftButton.wasPressedThisFrame) {
-                    startPos = clickAndDrag.getStartPos();
-                } else if(Mouse.current.leftButton.wasReleasedThisFrame) {
-                    startPos = clickAndDrag.getStartPos();
-                    endPos = clickAndDrag.getEndPos();
-
+        if (rb != null)
+        {
+            if (!isMoving())
+            {
+                if (!clickAndDrag.isDragging)
+                {
                     // Calculate the difference between where the ball starts and ends and uses it to create a vector.
-                    rb.linearVelocity = new Vector2((startPos.x - endPos.x) * forceAmount, (startPos.y - endPos.y) * forceAmount);
+                    rb.linearVelocity = new Vector2((clickAndDrag.startPos.x - clickAndDrag.endPos.x) * forceAmount, (clickAndDrag.startPos.y - clickAndDrag.endPos.y) * forceAmount);
                 }
+            }
+            else
+            {
+                clickAndDrag.endPos = clickAndDrag.startPos;
             }
         }
     }
 
     // Checks the ball velocity.
-    public bool isMoving() {
+    public bool isMoving()
+    {
         return rb.linearVelocity.magnitude != 0;
     }
 
-    public void setForce(float newForceAmount) {
+    public void setForce(float newForceAmount)
+    {
         forceAmount = newForceAmount;
     }
 
-    public float getForce() {
+    public float getForce()
+    {
         return forceAmount;
     }
 
@@ -70,20 +63,22 @@ public class LaunchWithDrag : MonoBehaviour
         get: float var = launchWithDrag.Mass;
         set: launchWithDrag.Mass = *some float val*;
     */
-
-    public float Mass {
+    public float Mass
+    {
         get { return rb.mass; }
         set { rb.mass = value; }
     }
     // For simulating objects moving through resistive mediums, like water or air.
     // Stay within 0.1-5
-    public float LinearDrag {
+    public float LinearDrag
+    {
         get { return rb.linearDamping; }
         set { rb.linearDamping = value; }
     }
     // Controls the resistance to rotational motion. Higher values slow down the rotation faster.
     // Stay within 0.05-1
-    public float AngularDrag {
+    public float AngularDrag
+    {
         get { return rb.angularDamping; }
         set { rb.angularDamping = value; }
     }
--- Assets/Scripts/Player/LineRendererController.cs
+++ Assets/Scripts/Player/LineRendererController.cs
@@ -1,48 +1,39 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class LineRendererController : MonoBehaviour
 {
     private LineRenderer lineRenderer;
     private ClickAndDrag clickAndDrag;
-    private LaunchWithDrag launchWithDrag;
-    private Vector3 endPos;
 
     void Start()
     {
         lineRenderer = gameObject.GetComponent<LineRenderer>();
         clickAndDrag = gameObject.GetComponent<ClickAndDrag>();
-        launchWithDrag = gameObject.GetComponent<LaunchWithDrag>();
         lineRenderer.enabled = false;
-        endPos = clickAndDrag.getEndPos();
         lineRenderer.SetPosition(0, gameObject.transform.position);
-        lineRenderer.SetPosition(1, endPos);
+        lineRenderer.SetPosition(1, clickAndDrag.endPos);
     }
 
     void Update()
     {
         if (lineRenderer != null)
         {
-            if(!launchWithDrag.isMoving()) {
-                // Changes the state of the line in real time
-                if(Mouse.current.leftButton.IsPressed()) {
-                    endPos = clickAndDrag.getEndPos();
-                }
-
-                // Set the start and end points and visibility of the Line Renderer
-                if(Mouse.current.leftButton.wasPressedThisFrame) {
+            if (clickAndDrag.startPos != clickAndDrag.endPos)
+            {
+                if (clickAndDrag.isDragging)
+                {
                     lineRenderer.enabled = true;
-
-                    // startPos = clickAndDrag.getStartPos();
-                } else if(Mouse.current.leftButton.wasReleasedThisFrame) {
-                    lineRenderer.enabled = false;
-
-                    // startPos = clickAndDrag.getStartPos();
-                    endPos = clickAndDrag.getEndPos();
                 }
+                else
+                {
+                    lineRenderer.enabled = false;
+                }
+
                 lineRenderer.SetPosition(0, gameObject.transform.position);
-                lineRenderer.SetPosition(1, endPos);
-            } else {
+                lineRenderer.SetPosition(1, clickAndDrag.endPos);
+            }
+            else
+            {
                 lineRenderer.enabled = false;
             }
         }
