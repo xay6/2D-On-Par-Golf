998f199ec5386492c0f4e0eefa3bb60a044bf5a5
Brandon Mason <bjm241@txstate.edu>
2025-02-18 15:05:34 -0600

    Script detects superimposition as well as general collisions. Added another variable that keeps track of all collisions.

--- Assets/Scripts/GameCode/CollisionDetection.cs
+++ Assets/Scripts/GameCode/CollisionDetection.cs
@@ -1,31 +1,49 @@
 using UnityEngine;
 
+/*
+    This class watches for collisions between the object this script is attatched to, and 
+    all objects that share the name specified in objectName(Value set within unity inspector).
+    
+    * onSuperimposed detects when the objects overlap.
+    * onCollision detects when the objects come into contact an any way.
+*/
 public class CollisionDetection : MonoBehaviour
 {
     public string objectName; // Must be set in Unity Inspector.
     private GameObject ball;
     private CircleCollider2D circleCollider2D;
-    public bool onCollisionDetected;
+    public bool onSuperimposed;
+    public bool onCollision;
     
 
     void Start() {
         ball = GameObject.Find(objectName);
         circleCollider2D = gameObject.GetComponent<CircleCollider2D>();
-        onCollisionDetected = false;
+        onSuperimposed = false;
+        onCollision = false;
     }
 
     void Update() {
         if(circleCollider2D != null && ball != null) {
             if(circleCollider2D.OverlapPoint(ball.transform.position)) {
-                onCollisionDetected = true;
+                onSuperimposed = true;
                 return;
+            }
+            if(circleCollider2D.IsTouching(ball.GetComponent<Collider2D>())) {
+                onCollision = true;
             }
         }
     }
 
+    public bool OnSuperimposed
+    {
+        get { return onSuperimposed; }
+        set { onSuperimposed = value; }
+    }
+
     public bool OnCollision
     {
-        get { return onCollisionDetected; }
-        set { onCollisionDetected = value; }
+        get { return onCollision; }
+        set { onCollision = value; }
     }
 }
