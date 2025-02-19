fde0bc080e1b9c6a47eba3ee184d975d593bd615
Brandon Mason <bjm241@txstate.edu>
2025-02-19 08:47:14 -0600

    Adapted existing code to work with LaunchWithDrag component's parameters.

--- Assets/Scripts/GameCode/GolfClubController.cs
+++ Assets/Scripts/GameCode/GolfClubController.cs
@@ -7,8 +7,10 @@ using System.Collections.Generic;
 public class GolfClub
 {
     public string clubName;
-    public float maxForce;
-    public float heightFactor;
+    public float forceMultiplier;
+    public float ballMass;
+    public float linearDamping;
+    public float angularDamping;
 }
 
 public class GolfClubController : MonoBehaviour
@@ -19,10 +21,13 @@ public class GolfClubController : MonoBehaviour
     private int currentClubIndex = 0;
 
     public GolfClub CurrentClub => clubs[currentClubIndex];
+    private LaunchWithDrag launchWithDrag;
 
     private void Start()
     {
+        launchWithDrag = GameObject.Find("Ball").GetComponent<LaunchWithDrag>();
         UpdateClubText();
+        UpdateClubStats();
     }
 
     private void UpdateClubText()
@@ -37,26 +42,35 @@ public class GolfClubController : MonoBehaviour
         }
     }
 
+    private void UpdateClubStats() {
+        launchWithDrag.setForce(CurrentClub.forceMultiplier);
+        launchWithDrag.AngularDrag = CurrentClub.angularDamping;
+        launchWithDrag.LinearDrag = CurrentClub.linearDamping;
+    }
+
     private void Update()
     {
         var keyboard = Keyboard.current;
-
+        
         if (keyboard.digit1Key.wasPressedThisFrame)
         {
             currentClubIndex = 0;
             UpdateClubText();
+            UpdateClubStats();
         }
 
         if (keyboard.digit2Key.wasPressedThisFrame)
         {
             currentClubIndex = 1;
             UpdateClubText();
+            UpdateClubStats();
         }
 
         if (keyboard.digit3Key.wasPressedThisFrame)
         {
             currentClubIndex = 2;
             UpdateClubText();
+            UpdateClubStats();
         }
     }
 }
