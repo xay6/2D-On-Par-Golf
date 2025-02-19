9eb1bab3f812062738ea74431893ee0178b365f9
Ariana Zapata <qzw10@txstate.edu>
2025-02-19 13:50:54 -0600

    LB/SU/LI skeleton files
    LB:Leaderboard, SU:SignUp, LI:Login

--- Assets/Scripts/Runtime/Metagame/Controllers/LeaderboardController.cs
+++ Assets/Scripts/Runtime/Metagame/Controllers/LeaderboardController.cs
@@ -1,6 +1,33 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
-public class LeaderboardController
+public class LeaderboardController : MonoBehaviour
 {
-    
+    [SerializeField] private LeaderboardView leaderboardView;
+
+    private void Awake()
+    {
+        leaderboardView.OnBack += HandleBack;
+        PopulateLeaderboard();
+    }
+
+    private void HandleBack()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    private void PopulateLeaderboard()
+    {
+        List<(string username, int score)> mockLeaderboard = new List<(string, int)>
+        {
+            ("Player1", 120),
+            ("Player2", 100),
+            ("Player3", 80),
+            ("Player4", 60),
+            ("Player5", 40)
+        };
+
+        leaderboardView.PopulateLeaderboard(mockLeaderboard);
+    }
 }
