5b7d4f2663f386d1285d7c752344a7d71d6aab0a
Xavier Ortiz <zbp6@txstate.edu>
2025-02-18 18:52:30 -0600

    Added a camera to follow the ball

---
+++ Assets/Scripts/GameCode/SmoothCameraFollow.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour {
+    
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float damping;
+
+    public Transform target;
+
+    private Vector3 vel = Vector3.zero;
+
+    private void FixedUpdate() {
+        Vector3 targetPosition = target.position + offset;
+        targetPosition.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref vel,damping);
+    }
+}
