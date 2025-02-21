f5eb3bcde1457a52e2cb3e740ba83897e7eeb665
natalie <jgf59@txstate.edu>
2025-02-21 17:17:34 -0600

    SCRUM-48 added score tracking feature to level 1 on Hole.cs script

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -51,7 +51,7 @@ public class ScoreManager : MonoBehaviour
     {
         if (scoreText != null)
         {
-            scoreText.text = $"Strokes: {strokes}";
+            scoreText.text = $"Strokes: {strokes}\n Overall Score: {overallScore}";
         }
     }
 }
