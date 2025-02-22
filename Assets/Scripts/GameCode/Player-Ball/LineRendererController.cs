a0c1c68a7784ee34e2b1fffe8a11c0a83d39b6b5
Brandon Mason <bjm241@txstate.edu>
2025-02-22 17:19:01 -0600

    SCRUM-13 The line script could render a line before the ball stopped moving. This made it look like a shot was being charged up but the ball wouldn't move.

--- Assets/Scripts/GameCode/Player-Ball/LineRendererController.cs
+++ Assets/Scripts/GameCode/Player-Ball/LineRendererController.cs
@@ -4,11 +4,13 @@ public class LineRendererController : MonoBehaviour
 {
     private LineRenderer lineRenderer;
     private ClickAndDrag clickAndDrag;
+    private Rigidbody2D rb;
 
     void Start()
     {
         lineRenderer = gameObject.GetComponent<LineRenderer>();
         clickAndDrag = gameObject.GetComponent<ClickAndDrag>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
         lineRenderer.enabled = false;
 
         // Ensure the LineRenderer has at least 2 positions before setting them
@@ -23,31 +25,24 @@ public class LineRendererController : MonoBehaviour
 
     void Update()
     {
-        if (lineRenderer != null && clickAndDrag != null)
+        if (lineRenderer != null && clickAndDrag != null && rb != null)
         {
-            if (clickAndDrag.startPos != clickAndDrag.endPos)
+            if (rb.linearVelocity.magnitude == 0 && !clickAndDrag.isDragging)
             {
-                if (clickAndDrag.isDragging)
+                if (clickAndDrag.isHovering())
                 {
                     lineRenderer.enabled = true;
+                    lineRenderer.SetPosition(0, clickAndDrag.startPos);
+                    lineRenderer.SetPosition(1, clickAndDrag.endPos);
                 }
                 else
                 {
                     lineRenderer.enabled = false;
                 }
-
-                // Ensure there are at least 2 positions before setting them
-                if (lineRenderer.positionCount < 2)
-                {
-                    lineRenderer.positionCount = 2;
-                }
-
-                lineRenderer.SetPosition(0, gameObject.transform.position);
-                lineRenderer.SetPosition(1, clickAndDrag.endPos);
             }
             else
             {
-                lineRenderer.enabled = false;
+                lineRenderer.SetPosition(1, clickAndDrag.endPos);
             }
         }
     }
