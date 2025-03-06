2ac8bbd80033a824f666ea709fb51b779ff1995f
Brandon Mason <bjm241@txstate.edu>
2025-03-06 08:56:49 -0600

    SCRUM-88: Initialized a method to connect to the login endpoint of the server's api.

---
+++ Assets/Scripts/Utilities/DatabaseRoutes.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnPar.Routers
+{
+    [System.Serializable]
+    public class LoginResponse
+    {
+        public string message;
+        public bool success;
+    }
+
+    public static class LoginRegister
+    {
+        // Post request
+        public static async Task<LoginResponse> LoginRoute(string username, string password)
+        {
+            string url = "https://on-par-server.onrender.com/api/users/login";
+            // string url = "localhost:3000/api/users/login"; // TODO: Local development string.
+            string userData = $"{{\"username\":\"{username}\",\"password\":\"{password}\"}}";
+
+            using (UnityWebRequest request = new UnityWebRequest(url, "POST"))
+            {
+                // Build request body.
+                byte[] bodyRaw = Encoding.UTF8.GetBytes(userData);
+                request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+                request.downloadHandler = new DownloadHandlerBuffer();
+                request.SetRequestHeader("Content-Type", "application/json");
+
+                var operation = request.SendWebRequest();
+                while (!operation.isDone)
+                {
+                    // Allows for asyncronous operation.
+                    // Keeps game from freezing while waiting for the request to complete.
+                    await Task.Yield();
+                }
+
+                // ConnectionError: No internet connection.
+                // ProtocolError: HTTP errors.
+                if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+                {
+                    Debug.LogError("Error: " + request.error);
+                    // return null;
+                }
+                else
+                {
+                    Debug.Log("Response: " + request.downloadHandler.text);
+                }
+
+                LoginResponse jsonResponse = JsonUtility.FromJson<LoginResponse>(request.downloadHandler.text);
+                return jsonResponse;
+            }
+        }
+    }
+}
