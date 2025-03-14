3516f48fd5bcd43e03c4ae6306f623e8d8a0eb90
Brandon Mason <bjm241@txstate.edu>
2025-03-14 15:30:00 -0500

    SCRUM-84: Added a method and endpoint for fetching a users score

--- backend/db/controllers/scoresController.ts
+++ backend/db/controllers/scoresController.ts
@@ -1,7 +1,40 @@
 import { Request, Response } from "express";
-import { CourseScores, IUserScores } from "../models/CourseScores"
+import CourseScores, { IUserScores } from "../models/CourseScores"
 import User from "../models/User";
 import mongoose from "mongoose";
+import { addScore } from "./leaderboardController";
+
+export const getScore = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { courseId, username } = req.body;
+
+        if (!courseId || !username) {
+            res.status(400).json({ message: 'Invalid input: courseId and username are required.', success: false });
+            return;
+        }
+
+        const user = await User.findOne({ username }).exec();
+        const scores = await CourseScores.findOne({ courseId }).populate('userData.user').exec();
+
+        if (scores && user) {
+            const userScore: IUserScores | undefined = scores.userData.find(score => {
+                if (score.user) return score.user._id.equals(user._id as mongoose.mongo.ObjectId);
+            });
+
+            if (userScore) {
+                res.status(200).json({ message: "Score fetched successfully.", userScore: { courseId, username, score: userScore.score }, success: true });
+                console.log({ message: "Score fetched successfully." });
+            } else {
+                res.status(404).json({ message: "User score not found.", success: false });
+                console.error("User score not found.");
+            }
+        }
+    } catch (err: any) {
+        res.status(500).json({ message: "An error occurred while adding the score.", success: false });
+        console.error("Error in getScore", err.message);
+    }
+}
+
 export const addUpdateScores = async (req: Request, res: Response): Promise<void> => {
     try {
         const { courseId, username, score } = req.body;
@@ -69,3 +102,21 @@ export const addUpdateScores = async (req: Request, res: Response): Promise<void
         console.error("Error in addOrUpdateScore", err.message);
     }
 }
+
+// export const createScore = async (req: Request, res: Response): Promise<void> => {
+//     try {
+//         const { courseId, username, score } = req.body;
+//         const user = await User.findOne({ username }).exec();
+
+//         const updatedCourse = await CourseScores.findOneAndUpdate(
+//             { courseId },
+//             { $addToSet: { userData: { user, score } } },
+//             { upsert: true, new: true, setDefaultsOnInsert: true }
+//         ).exec();
+
+//         res.status(200).json({ message: "Score created successfully.", updatedCourse });
+//     } catch (err: any) {
+//         res.status(500).json({ message: "An error occurred while adding the score." });
+//         console.error(err);
+//     }
+// }
