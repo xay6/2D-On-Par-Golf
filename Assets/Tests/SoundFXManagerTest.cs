25c341af0ea5a581b851f70f065918c372bd9853
natalie <jgf59@txstate.edu>
2025-04-12 22:00:02 -0500

    SCRUM-195 added test for soundFXManager

---
+++ Assets/Tests/SoundFXManagerTest.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class SoundFXManagerTest
+{
+    [Test]
+    public void PlaySoundEffect_CreatesAudioSource()
+    {
+        // Arrange
+        var soundManagerGO = new GameObject("SoundFXManager");
+        var soundManager = soundManagerGO.AddComponent<SoundFXManager>();
+
+        var target = new GameObject("Target");
+        var testClip = AudioClip.Create("TestClip", 44100, 1, 44100, false);
+
+        // Act
+        soundManager.PlaySoundEffect(testClip, target.transform, 1f);
+        var audioSource = target.GetComponent<AudioSource>();
+
+        // Assert
+        Assert.IsNotNull(audioSource);
+        Assert.AreEqual(testClip, audioSource.clip);
+        Assert.IsTrue(audioSource.isPlaying);
+    }
+}
