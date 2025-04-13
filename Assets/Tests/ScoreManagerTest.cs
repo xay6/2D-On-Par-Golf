29742ae82a69f0d9452ba2a20fa89b17fe82ec6e
natalie <jgf59@txstate.edu>
2025-04-12 21:48:57 -0500

    SCRUM-195 added test for scoremanager

---
+++ Assets/Tests/ScoreManagerTest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Scripts.GameCode.Managers;
+
+public class ScoreManagerTest
+{
+    [Test]
+    public void AddStroke_IncrementsStrokes()
+    {
+        // Setup
+        GameObject go = new GameObject("ScoreManager");
+        var manager = go.AddComponent<ScoreManager>();
+        ScoreManager.Instance = manager; // 👈 Assign manually
+
+        ScoreManager.Instance.strokes = 0;
+
+        // Act
+        ScoreManager.Instance.AddStroke();
+
+        // Assert
+        Assert.AreEqual(1, ScoreManager.Instance.strokes);
+    }
+}
