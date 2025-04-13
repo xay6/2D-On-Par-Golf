790e3294407a438c8d18c30570d2fcbd941891b9
Brandon Mason <bjm241@txstate.edu>
2025-04-12 22:01:13 -0500

    SCRUM-189: Unit testing execution results.

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,17 +1,23 @@
 module.exports = {
-    testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.test.ts'],
-    coverageDirectory: './coverage',
-    collectCoverageFrom: ['./tests/*.ts'],
-    transform: {
-        '^.+\\.ts$': 'ts-jest'
-    },
-    coveragePathIgnorePatterns: [
-      '/node_modules/',
-      '/tests/',
-      '/index.ts',
-      '/db/types.ts',
-      '.*\\.d\\.ts$'
-    ],
-    setupFilesAfterEnv: ['./tests/setup.ts']
-  };
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  coverageDirectory: './coverage',
+  collectCoverageFrom: ['./tests/*.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/tests/',
+    '/index.ts',
+    '/db/types.ts',
+    '.*\\.d\\.ts$'
+  ],
+  setupFilesAfterEnv: ['./tests/setup.ts'],
+  reporters: [
+    "default",
+    ["./node_modules/jest-html-reporter", {
+      "pageTitle": "Test Report"
+    }]
+  ]
+};
