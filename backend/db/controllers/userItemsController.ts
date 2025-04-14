0243b15495a0ef23dd6960a51e69d0e36b289759
Brandon Mason <bjm241@txstate.edu>
2025-04-14 16:26:07 -0500

    SCRUM-178: Add functions for retrieving a users coins and rewards.

--- backend/db/controllers/userItemsController.ts
+++ backend/db/controllers/userItemsController.ts
@@ -47,7 +47,9 @@ export const updateCoins = [authenticateJwt, async (req: AuthenticatedRequest, r
         console.log(`Coins changed for ${username}.`);
 
     } catch (err: any) {
-
+        console.error('Error in updateCoins:\n', err);
+        res.status(500).json({ message: `Internal server error: ${err.message}`, success: false });
+        return;
     }
 }]
 
@@ -94,6 +96,92 @@ export const updateRewards = [authenticateJwt, async (req: AuthenticatedRequest,
         console.log(`Items changed for ${username}.`);
 
     } catch (err: any) {
+        console.error('Error in updateRewards:\n', err);
+        res.status(500).json({ message: `Internal server error: ${err.message}`, success: false });
+        return;
+    }
+}]
 
+export const getCoins = [authenticateJwt, async (req: AuthenticatedRequest, res: Response) => {
+    try {
+        const { username } = req.body;
+        if(!username) {
+            res.status(422).json({ message: "Invalid input: Username is required.", success: false });
+            return;
+        }
+
+        const user = await User.findOne({ username });
+        if (!user) {
+            res.status(404).json({ message: "User not found.", success: false });
+            console.error("User not found.");
+            return;
+        }
+
+        if (req.user?.username !== username && req.user?.id !== user._id) {
+            res.status(403).json({ message: "You are not authorized to fetch this users coins.", success: false });
+            console.error("You are not authorized to fetch this users coins.");
+            return;
+        }
+
+        const userItems = await UserItems.findOne({ user });
+        if (!userItems) {
+            const newUserItems = new UserItems({
+                user,
+                coinAmount: 0,
+            });
+            await newUserItems.save();
+            res.status(200).json({ message: `Coins created for ${username}.`, success: true });
+            console.log(`Coins created for ${username}.`);
+            return;
+        }
+
+        res.status(200).json({ message: `${username} has ${userItems.coinAmount}.`, coinAmount: userItems.coinAmount, success: true });
+        console.log(`Items fetched for ${username}.`);
+    } catch (err: any) {
+        console.error('Error in updateRewards:\n', err);
+        res.status(500).json({ message: `Internal server error: ${err.message}`, success: false });
+        return;
+    }
+}]
+
+export const getRewards = [authenticateJwt, async (req: AuthenticatedRequest, res: Response) => {
+    try {
+        const { username } = req.body;
+        if(!username) {
+            res.status(422).json({ message: "Invalid input: Username is required.", success: false });
+            return;
+        }
+
+        const user = await User.findOne({ username });
+        if (!user) {
+            res.status(404).json({ message: "User not found.", success: false });
+            console.error("User not found.");
+            return;
+        }
+
+        if (req.user?.username !== username && req.user?.id !== user._id) {
+            res.status(403).json({ message: "You are not authorized to fetch the users items.", success: false });
+            console.error("You are not authorized to fetch this users items.");
+            return;
+        }
+
+        const userItems = await UserItems.findOne({ user });
+        if (!userItems) {
+            const newUserItems = new UserItems({
+                user,
+                rewards: []
+            });
+            await newUserItems.save();
+            res.status(200).json({ message: `Rewards created for ${username}.`, success: true });
+            console.log(`Rewards created for ${username}.`);
+            return;
+        }
+
+        res.status(200).json({ message: `Items fetched for ${username}.`, rewards: userItems.rewards, success: true });
+        console.log(`Items fetched for ${username}.`);
+    } catch (err: any) {
+        console.error('Error in updateRewards:\n', err);
+        res.status(500).json({ message: `Internal server error: ${err.message}`, success: false });
+        return;
     }
 }]
