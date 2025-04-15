9ebb911bbfe56de4053fa6ce03b6a5c67b60e53b
natalie <jgf59@txstate.edu>
2025-04-14 23:40:41 -0500

    SCRUM-158 created volume settings script

---
+++ Assets/Scripts/GameCode/UI/VolumeSetings.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer MainMixer;
+    [SerializeField] private Slider musicVolume;
+    [SerializeField] private Slider soundFXVolume;
+
+
+    public void setMusicVolume()
+    {
+        float volume= musicVolume.value;
+        MainMixer.SetFloat("music", Mathf.Log10(volume)*20);
+    }
+    public void setSoundFXVolume()
+    {
+        float volume= soundFXVolume.value;
+        MainMixer.SetFloat("soundFX", Mathf.Log10(volume)*20);
+    }
+}
