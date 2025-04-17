8f17c7a1931059608ae3db4695b2e08cb64701e8
Ariana Zapata <qzw10@txstate.edu>
2025-04-17 06:42:19 -0500

    SCRUM-149
    Added script files for AllLevelsVIew

--- Assets/Scripts/Runtime/Metagame/Controllers/AllLevelsController.cs
+++ Assets/Scripts/Runtime/Metagame/Controllers/AllLevelsController.cs
@@ -1,54 +1,9 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
-using Unity.Template.Multiplayer.NGO.Runtime;
-using OnPar.Routers;
-using OnPar.RouterHandlers;
 
-internal class AllLevelsController : Controller<MetagameApplication>
+namespace Unity.Template.Multiplayer.NGO.Runtime
 {
-    AllLevelsView View => App.View.AllLevels;
-
-    void Awake()
+    public class AllLevelsController
     {
-        AddListener<EnterAllLevelsEvent>(OnEnter);
-        AddListener<ExitAllLevelsEvent>(OnExit);
-    }
-
-    async void OnEnter(EnterAllLevelsEvent evt)
-    {
-        string username = LoginRegister.getUsername();
-        var response = await Handlers.GetAllUserScoresHandler(username);
-
-        if (response != null && response.success)
-        {
-            var levels = new List<LevelData>();
-            foreach (var scoreEntry in response.scores)
-            {
-                levels.Add(new LevelData
-                {
-                    courseId = scoreEntry.courseId,
-                    score = scoreEntry.score
-                });
-            }
-
-            View.Initialize(levels);
-            View.Show();
-        }
-        else
-        {
-            Debug.LogError("Failed to load level data from server.");
-        }
-    }
-
-    void OnExit(ExitAllLevelsEvent evt)
-    {
-        View.Hide();
-    }
-
-    internal override void RemoveListeners()
-    {
-        RemoveListener<EnterAllLevelsEvent>(OnEnter);
-        RemoveListener<ExitAllLevelsEvent>(OnExit);
+    
     }
 }
--- Assets/Scripts/Runtime/Metagame/Views/AllLevelsView.cs
+++ Assets/Scripts/Runtime/Metagame/Views/AllLevelsView.cs
@@ -1,87 +1,9 @@
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace Unity.Template.Multiplayer.NGO.Runtime
 {
-    internal class AllLevelsView : View<MetagameApplication>
+    public class AllLevelsView
     {
-        ScrollView m_LevelList;
-        Button m_BackButton;
-        Button m_NextButton;
-        Button m_PrevButton;
-        VisualElement m_Root;
-        UIDocument m_UIDocument;
-
-        List<LevelData> m_AllLevels;
-        int m_UnlockedUpTo;
-        int m_LevelsPerPage = 6;
-        int m_CurrentPage = 0;
-
-        void Awake()
-        {
-             m_UIDocument = GetComponent<UIDocument>();
-            if (m_UIDocument != null)
-            {
-                m_Root = m_UIDocument.rootVisualElement;
-            }
-
-            m_LevelList = m_Root.Q<ScrollView>("levelList");
-            m_BackButton = m_Root.Q<Button>("backButton");
-            m_NextButton = m_Root.Q<Button>("nextButton");
-            m_PrevButton = m_Root.Q<Button>("prevButton");
-
-            m_BackButton.clicked += () => Broadcast(new ExitAllLevelsEvent());
-            m_NextButton.clicked += ShowNextPage;
-            m_PrevButton.clicked += ShowPreviousPage;
-        }
-
-        public void Initialize(List<LevelData> allLevels)
-        {
-            m_AllLevels = allLevels;
-            m_CurrentPage = 0;
-            RenderPage();
-        }
-
-        void RenderPage()
-        {
-            m_LevelList.Clear();
-
-            int start = m_CurrentPage * m_LevelsPerPage;
-            int end = Mathf.Min(start + m_LevelsPerPage, m_AllLevels.Count);
-
-            for (int i = start; i < end; i++)
-            {
-                var level = m_AllLevels[i];
-                var button = new Button();
-                button.text = $"Level {level.courseId}";
-
-                if (level.isUnlocked)
-                {
-                    button.SetEnabled(true);
-                    int capturedId = i; // fix closure issue
-                    button.clicked += () => LoadLevel(m_AllLevels[capturedId].courseId);
-                }
-                else
-                {
-                    button.SetEnabled(false);
-                    button.text += " (Locked)";
-                }
-
-                m_LevelList.Add(button);
-            }
-
-            m_PrevButton.SetEnabled(m_CurrentPage > 0);
-            m_NextButton.SetEnabled((m_CurrentPage + 1) * m_LevelsPerPage < m_AllLevels.Count);
-        }
-
-
-        void ShowNextPage() { m_CurrentPage++; RenderPage(); }
-        void ShowPreviousPage() { m_CurrentPage--; RenderPage(); }
-
-        void LoadLevel(string sceneName)
-        {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
-        }
+    
     }
 }
