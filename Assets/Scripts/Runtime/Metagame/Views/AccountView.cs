972683294633086ff62a15c43cb7a8187fd5d1f3
Xavier Ortiz <zbp6@txstate.edu>
2025-04-21 22:08:25 -0500

    Added a path to get to the rewards scene

--- Assets/Scripts/Runtime/Metagame/Views/AccountView.cs
+++ Assets/Scripts/Runtime/Metagame/Views/AccountView.cs
@@ -53,8 +53,8 @@ namespace Unity.Template.Multiplayer.NGO.Runtime
         void OnNewGameClicked(ClickEvent evt) => Broadcast(new StartGameEvent());
         void OnAllLevelsClicked(ClickEvent evt) => Broadcast(new EnterLeaderboardEvent()); // swap if needed
         void OnSettingsClicked(ClickEvent evt) => Broadcast (new EnterAccountSettingsEvent());
-        void OnRewardsClicked(ClickEvent evt) => Debug.Log("Rewards clicked"); // replace with actual event
-        void OnChallengesClicked(ClickEvent evt) => SceneManager.LoadScene("ChallengeLevel01"); // replace with actual event
+        void OnRewardsClicked(ClickEvent evt) => SceneManager.LoadScene("RewardScene"); 
+        void OnChallengesClicked(ClickEvent evt) => SceneManager.LoadScene("ChallengeLevel01"); 
         void OnMainMenuClicked(ClickEvent evt) => Broadcast(new ExitAccountEvent());
     }
 }
